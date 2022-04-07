a85e042e66c1df9e6cdce1ac3f84f88144a00b3a
PyGamer0 <maanasyt@gmail.com>
2022-04-07 12:04:17 +0530

    u

--- lua/conf/cmds.lua
+++ lua/conf/cmds.lua
@@ -3,5 +3,5 @@ autocmd VimLeave * set guicursor=a:ver25
 autocmd BufReadPost * silent! normal! g`"zv
 autocmd TextYankPost * lua vim.highlight.on_yank{higroup="IncSearch", timeout=150, on_visual=true}
 autocmd FileType make set noexpandtab
-autocmd FileType * RainbowParentheses
+autocmd FileType python,c,cpp,javascript execute ':RainbowParentheses'
 ]])
